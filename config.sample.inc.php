4aa40beebda3528f03a9e914beb7251c890a33fc
infinex-exchange <admin@infinex.cc>
2023-09-25 14:23:41 +0200

    amqp username

--- config.sample.inc.php
+++ config.sample.inc.php
@@ -4,7 +4,7 @@ define('LOG_LEVEL', 0);
 
 define('AMQP_HOST', 'localhost');
 define('AMQP_PORT', 5672);
-define('AMQP_USER', 'logsink');
+define('AMQP_USER', 'mon.logsink');
 define('AMQP_PASS', 'password');
 define('AMQP_VHOST', '/');
 
